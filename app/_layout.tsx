a41349f122ccbf5ed68e267ea80ef2f2e83ca348
yslanio <dev.yslanio@gmail.com>
2026-03-23 14:06:25 -0300

    algum espacamento

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/components/useColorScheme';
+import { useColorScheme } from "@/components/useColorScheme";
 
-import { AuthProvider } from '../src/context/AuthContext';
+import { AuthProvider } from "../src/context/AuthContext";
 
-export {
-  ErrorBoundary,
-} from 'expo-router';
+export { ErrorBoundary } from "expo-router";
 
 export const unstable_settings = {
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -50,21 +52,21 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
         {/* Rota principal (Tabs ou Onboarding) */}
         <Stack.Screen name="(tabs)" />
-        
+
         {/* Rotas de Autenticação */}
-        <Stack.Screen name="auth/login" options={{ title: 'Login' }} />
-        <Stack.Screen name="auth/register" options={{ title: 'Cadastro' }} />
-        
+        <Stack.Screen name="auth/login" options={{ title: "Login" }} />
+        <Stack.Screen name="auth/register" options={{ title: "Cadastro" }} />
+
         {/* Rotas de Destino Pós-Login */}
         <Stack.Screen name="app\(protected)\mc\(tabs)\index.tsx" />
         <Stack.Screen name="app\(protected)\audience\(tabs)\index.tsx" />
         <Stack.Screen name="app\(protected)\organizer\(tabs)\index.tsx" />
 
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
   );
